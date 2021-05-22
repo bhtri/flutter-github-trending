0375e553fdc3e8e512ff08d8e851d5fe8823ca88
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-05-22 19:29:23 +0900

    🔧 fix url github api

--- lib/resources/git_trend_api_provider.dart
+++ lib/resources/git_trend_api_provider.dart
@@ -9,15 +9,20 @@ class GitTrendApiProvider {
     String since,
     String spokenLanguage,
   ) async {
-    String url =
-        'https://ghapi.huchen.dev/repositories?language=$language&since=$since&spoken_language_code=$spokenLanguage';
-    Response response = await dio.get(url);
+    try {
+      // https://github.com/huchenme/github-trending-api/issues/130#issuecomment-706741619
+      String url =
+          'https://hackertab.pupubird.com/repositories?language=$language&since=$since&spoken_language_code=$spokenLanguage';
+      Response response = await dio.get(url);
 
-    if (response.statusCode == 200) {
-      Iterable list = response.data;
-      return list.map((json) => RepositoryModel.fromJson(json)).toList();
+      if (response.statusCode == 200) {
+        Iterable list = response.data;
+        return list.map((json) => RepositoryModel.fromJson(json)).toList();
+      }
+
+      throw Exception('Failed to load get');
+    } catch (e) {
+      throw Exception(e.toString());
     }
-
-    throw Exception('Failed to load get');
   }
 }
