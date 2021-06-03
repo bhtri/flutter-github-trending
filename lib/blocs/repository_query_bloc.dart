5a99e9b36ad7ec82304cbc204d633bc9606dfd47
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-06-03 15:42:45 +0900

    ➕ bloc refactoring

--- lib/blocs/repository_query_bloc.dart
+++ lib/blocs/repository_query_bloc.dart
@@ -1,1 +1,30 @@
-class RepositoryQueryBloc {}
+import 'package:gittrend/models/repository_model.dart';
+import 'package:gittrend/models/repository_query_model.dart';
+import 'package:gittrend/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+
+class RepositoryQueryBloc {
+  final repository = Repository();
+  final repositoryQuery = BehaviorSubject<RepositoryQueryModel>();
+  final trendingRepositoriesFetcher =
+      BehaviorSubject<Future<List<RepositoryModel>>>();
+
+  void fetchRepositoryQuery(RepositoryQueryModel queryModel) {
+    repositoryQuery.sink.add(queryModel);
+  }
+
+  RepositoryQueryBloc() {
+    // repositoryQuery.stream.transform().pipe(trendingRepositoriesFetcher);
+  }
+
+  queryTransformer() {}
+
+  dispose() async {
+    await repositoryQuery.drain();
+    repositoryQuery.close();
+
+    await trendingRepositoriesFetcher.drain();
+    trendingRepositoriesFetcher.close();
+  }
+}
